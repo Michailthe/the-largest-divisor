defcbabf872e4e92a5d2b0f582196b595561598a
Michailthe <112062292+Michailthe@users.noreply.github.com>
2024-10-19 15:33:08 +0300

    Добавьте файлы проекта.

---
+++ Наибольший делитель/Наибольший делитель.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int findGCD(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int main() {
+    int num1, num2;
+    cout << "Введите два числа: ";
+    cin >> num1 >> num2;
+    int gcd = findGCD(num1, num2);
+
+    cout << "НОД(" << num1 << ", " << num2 << ") = " << gcd << endl;
+
+    return 0;
+}
